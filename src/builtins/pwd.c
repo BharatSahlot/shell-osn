73b13f4170426ba405c48b7e06da9c2d400ee4df
BharatSahlot <bharatsahlot18@gmail.com>
2022-09-01 23:25:29 +0530

    pwd works

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,3 +1,13 @@
 #include "builtins.h"
 
-int pwd(int argc, const char **argv) {}
+#include <stdio.h>
+
+int pwd(int argc, const char **argv)
+{
+    if(argc > 1)
+    {
+        Log(LOGL_WARN, "pwd: too many arguements\n");
+    }
+    printf("%s\n", currentPath);
+    return 0;
+}
