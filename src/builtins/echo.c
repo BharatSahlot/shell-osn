a7d83fbe7b15f463285d7d1cc8219b3653d349ae
BharatSahlot <bharatsahlot18@gmail.com>
2022-09-01 22:34:39 +0530

    echo done

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,3 +1,14 @@
 #include "builtins.h"
 
-int echo(int argc, const char **argv) {}
+#include <stdio.h>
+
+int echo(int argc, const char **argv)
+{
+    for(int i = 1; i < argc; i++)
+    {
+        if(i > 1) printf(" ");
+        printf("%s", argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
